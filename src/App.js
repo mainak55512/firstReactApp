e282a8a257ed691765d32ec6bdcd4a466128b651
mainak55512 <mbhattacharjee432@gmail.com>
2022-05-22 23:20:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
-function App() {
+let uname = ["User", "User"];
+
+function Child(props) {
+
+  if (props.info.log) {
+    uname[1] = uname[0];
+    uname[0] = props.info.data;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {
+        props.info.log ?
+          <h1 id="childStyle" >Hello {uname[0]}!!! Nice to meet you {":)"}</h1>
+          :
+          <h1 id="childStyle" >Hello {uname[1]}!!! Nice to meet you {":)"}</h1>
+      }
+    </>
   );
 }
 
-export default App;
+export function App() {
+  const [data, setData] = useState("");
+  const [log, setLog] = useState(false);
+  const dataInfo = { data: data, log: log }
+
+  function getVal(event) {
+    setData(event.target.value);
+    setLog(false);
+  }
+
+
+
+  return (
+    <>
+      <Child info={dataInfo} />
+      <input type="text" onChange={getVal} id = "inpStyle" placeholder="Enter Name Here"/>
+      <button onClick={() => setLog(true)} id = "buttonStyle">log</button>
+    </>
+  );
+}
