e3b50cc5f9c23228ae0e1141f5ba1d691c1fdb76
mainak55512 <mbhattacharjee432@gmail.com>
2022-05-29 17:58:27 +0530

    Updates

---
+++ precache-manifest.122cbbd92dd5abd07c135bb7ab83f3dc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "55898096e9406f45d2f9",
+    "url": "/firstReactApp/static/css/main.005d4b11.chunk.css"
+  },
+  {
+    "revision": "55898096e9406f45d2f9",
+    "url": "/firstReactApp/static/js/main.55898096.chunk.js"
+  },
+  {
+    "revision": "4e9aafb99959cf9fe670",
+    "url": "/firstReactApp/static/js/1.4e9aafb9.chunk.js"
+  },
+  {
+    "revision": "65798ea548323e7b0568",
+    "url": "/firstReactApp/static/css/2.0ade7fba.chunk.css"
+  },
+  {
+    "revision": "65798ea548323e7b0568",
+    "url": "/firstReactApp/static/js/2.65798ea5.chunk.js"
+  },
+  {
+    "revision": "80bcae67caa251b292a3",
+    "url": "/firstReactApp/static/js/runtime~main.80bcae67.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/firstReactApp/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "2899d4a27687b740ed87012fadff65f4",
+    "url": "/firstReactApp/index.html"
+  }
+];
